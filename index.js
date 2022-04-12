951303f247fbf556176ab661e888b307781360cb
Gwinyai Chakonda <gtchax@gmail.com>
2022-04-12 16:11:02 +0200

    debuggin deployment

--- index.js
+++ index.js
@@ -29,14 +29,19 @@ app.use(compression());
 app.use(helmet());
 app.set("x-powered-by", false);
 
-if (process.env.NODE_ENV === "production") {
-  // Step 1:
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
-  // Step 2:
-  app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build"));
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   // Step 1:
+//   app.use(express.static(path.resolve(__dirname, "/client/build")));
+//   // Step 2:
+//   app.get("*", function (request, response) {
+//     response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 //--- Mounting routes
 app.use("/api/v1", require("./routes/auth.routes"));
